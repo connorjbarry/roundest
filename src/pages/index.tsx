193a6461ebc154d2c2d7c112fec3f12f99a754f7
connorjbarry <barry31@purdue.edu>
2022-09-12 21:03:36 -0400

    backend w pokemon data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,14 +12,41 @@ const Home: NextPage = () => {
 
   const [first, second] = pokemon;
 
+  const firstPokemon = trpc.useQuery([
+    "pokemon.get-pokemon-by-id",
+    { id: first! },
+  ]);
+  const secondPokemon = trpc.useQuery([
+    "pokemon.get-pokemon-by-id",
+    { id: second! },
+  ]);
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-400">{first}</div>
+        <div className="w-64 h-64">
+          <img
+            src={firstPokemon.data?.sprites.front_default}
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {firstPokemon.data?.name}
+          </div>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-400">{second}</div>
+        <div className="w-64 h-64">
+          <img
+            src={secondPokemon.data?.sprites.front_default}
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {secondPokemon.data?.name}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/server/router/pokemonRouter.ts
@@ -0,0 +1,14 @@
+import * as trpc from "@trpc/server";
+import { z } from "zod";
+
+import { PokemonClient } from "pokenode-ts";
+
+export const pokemonRouter = trpc.router().query("get-pokemon-by-id", {
+  input: z.object({ id: z.number() }),
+  async resolve({ input }) {
+    const api = new PokemonClient();
+
+    const pokemon = await api.getPokemonById(input.id);
+    return pokemon;
+  },
+});
