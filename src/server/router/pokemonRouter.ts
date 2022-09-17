1dd4bbffd748d8a7ac5317cfe6b37a8615d13167
connorjbarry <barry31@purdue.edu>
2022-09-16 19:12:07 -0500

    Add endpoint for voting

--- src/server/router/pokemonRouter.ts
+++ src/server/router/pokemonRouter.ts
@@ -1,14 +1,28 @@
 import * as trpc from "@trpc/server";
 import { z } from "zod";
+import { prisma } from "../db/client";
 
 import { PokemonClient } from "pokenode-ts";
 
-export const pokemonRouter = trpc.router().query("get-pokemon-by-id", {
-  input: z.object({ id: z.number() }),
-  async resolve({ input }) {
-    const api = new PokemonClient();
+export const pokemonRouter = trpc
+  .router()
+  .query("get-pokemon-by-id", {
+    input: z.object({ id: z.number() }),
+    async resolve({ input }) {
+      const api = new PokemonClient();
 
-    const pokemon = await api.getPokemonById(input.id);
-    return { name: pokemon.name, sprites: pokemon.sprites };
-  },
-});
+      const pokemon = await api.getPokemonById(input.id);
+      return { name: pokemon.name, sprites: pokemon.sprites };
+    },
+  })
+  .mutation("cast-vote", {
+    input: z.object({ votedFor: z.number(), votedAgainst: z.number() }),
+    async resolve({ input }) {
+      const voteInDb = await prisma.vote.create({
+        data: {
+          ...input,
+        },
+      });
+      return { success: true, vote: voteInDb };
+    },
+  });
